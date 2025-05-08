f4fc7ae5ee4a08f2858119b9616dc0fa49765871
vaishnavi290502 <vaishnu9390@gmail.com>
2025-05-08 23:29:11 +0530

    Update bmsCode.py

--- bmsCode.py
+++ bmsCode.py
@@ -91,7 +91,7 @@ def main():
         with tab2:
             st.subheader("Analyse Data")
             st.write("Redirecting to Analyse Data...")
-            st.markdown('<a href="https://bmsgcek-5vwhdph3tjqhgfzstsmt6gcek.streamlit.app///" target="_blank">Click here if not redirected</a>', unsafe_allow_html=True)
+            st.markdown('<a href="https://bmsanalyse-nme2mphcztfjwmjqyncpef.streamlit.app//" target="_blank">Click here if not redirected</a>', unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
